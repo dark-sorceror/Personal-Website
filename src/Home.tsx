cc8efa84e09f19dd2c88265e3f69ce399b0813c1
Hao Yan <haoyan160@gmail.com>
2025-11-05 21:06:13 -0500

    Finished hero section and added light dark mode

--- src/Home.tsx
+++ src/Home.tsx
@@ -4,10 +4,14 @@ export default function Home() {
             <section className="p1">
                 <div className="p1w">
                     <div className="top">Hi, my name is</div>
-                    <div className="name">Hao Yan</div>
+                    <div className="name">Hao Yan.</div>
+                    <div className="tagline">Bridging intelligence and autonomy.</div>
                     <div className="desc">
-                        desc
+                        A university student interested in AI/ML and robotics
                     </div>
+                </div>
+                <div className="background-dots">
+                    <div></div>
                 </div>
             </section>
             <section className="p2">p2</section>
--- src/components/Linkbar/Linkbar.tsx
+++ src/components/Linkbar/Linkbar.tsx
@@ -1,6 +1,21 @@
+import { useEffect, useState } from "react";
+
 import "./Linkbar.css";
 
 export default function Linkbar() {
+    const [darkMode, setDarkMode] = useState(() => {
+        const userDefault = localStorage.getItem("darkMode");
+
+        return userDefault === "true";
+    });
+
+    useEffect(() => {
+        document.body.classList.toggle("dark-mode", darkMode);
+        localStorage.setItem("darkMode", `${darkMode}`);
+    }, [darkMode]);
+
+    const toggleDarkMode = () => setDarkMode((prev) => !prev);
+
     return (
         <div className="linkbar-wrapper">
             <div className="linkbar">
@@ -60,6 +75,48 @@ export default function Linkbar() {
                         <rect x="2" y="4" width="20" height="16" rx="2" />
                     </svg>
                 </a>
+                <div className="border"></div>
+                <button
+                    className="ld-mode"
+                    onClick={toggleDarkMode}
+                >
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="24"
+                        height="24"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        stroke="currentColor"
+                        stroke-width="2"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        className="lucide lucide-moon-icon lucide-moon"
+                    >
+                        <path d="M20.985 12.486a9 9 0 1 1-9.473-9.472c.405-.022.617.46.402.803a6 6 0 0 0 8.268 8.268c.344-.215.825-.004.803.401" />
+                    </svg>
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="24"
+                        height="24"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        stroke="currentColor"
+                        stroke-width="2"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        className="lucide lucide-sun-icon lucide-sun"
+                    >
+                        <circle cx="12" cy="12" r="4" />
+                        <path d="M12 2v2" />
+                        <path d="M12 20v2" />
+                        <path d="m4.93 4.93 1.41 1.41" />
+                        <path d="m17.66 17.66 1.41 1.41" />
+                        <path d="M2 12h2" />
+                        <path d="M20 12h2" />
+                        <path d="m6.34 17.66-1.41 1.41" />
+                        <path d="m19.07 4.93-1.41 1.41" />
+                    </svg>
+                </button>
             </div>
         </div>
     );
