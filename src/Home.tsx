49c0a0894b852ea773b9b68d87a46b3be3da2dae
Hao Yan <haoyan160@gmail.com>
2025-11-08 00:18:00 -0500

    Work on experience section

--- src/Home.tsx
+++ src/Home.tsx
@@ -19,10 +19,40 @@ export default function Home() {
             <section className="p2">
                 <div className="p2w">
                     <div className="p2header">Experiences</div>
-                    <div className="design"></div>
                     <div className="p2e1">
                         <div className="details">
                             <div className="title">Title</div>
+                            <div className="second">
+                                <div className="tags">
+                                    <div className="tag">
+                                        <span>tag 1</span>
+                                    </div>
+                                    <div className="tag">
+                                        <span>tag 2</span>
+                                    </div>
+                                </div>
+                                <div className="date">
+                                    <i>Ongoing</i>
+                                </div>
+                            </div>
+                            <div className="location">
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    width="24"
+                                    height="24"
+                                    viewBox="0 0 24 24"
+                                    fill="none"
+                                    stroke="currentColor"
+                                    stroke-width="2"
+                                    stroke-linecap="round"
+                                    stroke-linejoin="round"
+                                >
+                                    <circle cx="12" cy="12" r="10" />
+                                    <path d="M12 2a14.5 14.5 0 0 0 0 20 14.5 14.5 0 0 0 0-20" />
+                                    <path d="M2 12h20" />
+                                </svg>
+                                <span>Canada</span>
+                            </div>
                             <div className="p2edesc">Desc</div>
                         </div>
                         <div className="placeholder"></div>
@@ -31,12 +61,74 @@ export default function Home() {
                         <div className="placeholder"></div>
                         <div className="details">
                             <div className="title">Title</div>
+                            <div className="second">
+                                <div className="tags">
+                                    <div className="tag">
+                                        <span>tag 1</span>
+                                    </div>
+                                    <div className="tag">
+                                        <span>tag 2</span>
+                                    </div>
+                                </div>
+                                <div className="date">
+                                    <i>Sept 2024 - Aug 2025</i>
+                                </div>
+                            </div>
+                            <div className="location">
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    width="24"
+                                    height="24"
+                                    viewBox="0 0 24 24"
+                                    fill="none"
+                                    stroke="currentColor"
+                                    stroke-width="2"
+                                    stroke-linecap="round"
+                                    stroke-linejoin="round"
+                                >
+                                    <circle cx="12" cy="12" r="10" />
+                                    <path d="M12 2a14.5 14.5 0 0 0 0 20 14.5 14.5 0 0 0 0-20" />
+                                    <path d="M2 12h20" />
+                                </svg>
+                                <span>Canada</span>
+                            </div>
                             <div className="p2edesc">Desc</div>
                         </div>
                     </div>
                     <div className="p2e3">
                         <div className="details">
                             <div className="title">Title</div>
+                            <div className="second">
+                                <div className="tags">
+                                    <div className="tag">
+                                        <span>tag 1</span>
+                                    </div>
+                                    <div className="tag">
+                                        <span>tag 2</span>
+                                    </div>
+                                </div>
+                                <div className="date">
+                                    <i>Feb - April 2019</i>
+                                </div>
+                            </div>
+                            <div className="location">
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    width="24"
+                                    height="24"
+                                    viewBox="0 0 24 24"
+                                    fill="none"
+                                    stroke="currentColor"
+                                    stroke-width="2"
+                                    stroke-linecap="round"
+                                    stroke-linejoin="round"
+                                >
+                                    <circle cx="12" cy="12" r="10" />
+                                    <path d="M12 2a14.5 14.5 0 0 0 0 20 14.5 14.5 0 0 0 0-20" />
+                                    <path d="M2 12h20" />
+                                </svg>
+                                <span>Canada</span>
+                            </div>
                             <div className="p2edesc">Desc</div>
                         </div>
                         <div className="placeholder"></div>
