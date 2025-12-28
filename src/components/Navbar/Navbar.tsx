06b8544ebe301ecd7079c8aa70aa0f20959ca702
Hao Yan <haoyan160@gmail.com>
2025-12-27 22:17:38 -0700

    Remake navigation bar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,18 +1,51 @@
+import { useState, useRef, useEffect } from "react";
+
 import "./Navbar.css";
 
+const tabs = ["About Me", "Experience", "Projects", "Contact"];
+
 export default function Navbar() {
+    const [activeIndex, setActiveIndex] = useState(0);
+    const [indicatorStyle, setIndicatorStyle] = useState({ left: 0, width: 0 });
+
+    const tabsRef = useRef<(HTMLAnchorElement | null)[]>([]);
+
+    useEffect(() => {
+        const currentTab = tabsRef.current[activeIndex];
+
+        if (currentTab) {
+            setIndicatorStyle({
+                left: currentTab.offsetLeft,
+                width: currentTab.offsetWidth,
+            });
+        }
+    }, [activeIndex]);
+
     return (
         <nav>
             <div className="nav">
-                <div className="logo"></div>
-                <div className="hotlinks">
-                    <a href="" className="about-me">
-                        About Me
+                <div
+                    className="indicator"
+                    style={{
+                        left: `${indicatorStyle.left}px`,
+                        width: `${indicatorStyle.width}px`,
+                    }}
+                />
+
+                {tabs.map((tab, index) => (
+                    <a
+                        key={tab}
+                        ref={(el) => {
+                            tabsRef.current[index] = el;
+                        }}
+                        className={`nav-item ${
+                            activeIndex === index ? "active" : ""
+                        }`}
+                        onClick={() => setActiveIndex(index)}
+                    >
+                        {tab}
                     </a>
-                    <a href="" className="experience">
-                        Experience
-                    </a>
-                </div>
+                ))}
             </div>
         </nav>
     );
