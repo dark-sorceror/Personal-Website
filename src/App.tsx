1f3bbd7e13cac5b79ef6a1653ce0a9e2f0a339f1
Hao Yan <haoyan160@gmail.com>
2026-01-25 00:38:15 -0500

    Minimal version
    Until the full one is finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,11 @@
 import Home from "./Home";
 
-import Linkbar from "./components/Linkbar";
-import Navbar from "./components/Navbar";
-import StarBackground from "./components/StarBackground";
-
 import "./App.css";
-import BTT from "./components/BTT";
 
 function App() {
     return (
         <>
-            <StarBackground />
-            <Navbar />
-            <Linkbar />
             <Home />
-            <BTT />
         </>
     );
 }
